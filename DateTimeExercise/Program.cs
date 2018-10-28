c7b9e9903a362faee96ce685e883a8997b3d7354
William B McAllister <blakemcallister77@gmail.com>
2018-10-28 19:59:45 -0400

    pg 156

--- DateTimeExercise/Program.cs
+++ DateTimeExercise/Program.cs
@@ -9,7 +9,14 @@ namespace DateTimeExercise
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{	
+			Console.WriteLine("The current Date and Time is now: " + DateTime.Now);
+			Console.WriteLine("Please pick a number between 1-12.");
+			int userNum = Convert.ToInt32(Console.ReadLine());
+			DateTime dateTime = DateTime.Now;
+			Console.WriteLine("The current date and time plus the amount of hours you added is: {0} " , dateTime.AddHours(userNum));
+			Console.ReadLine();
+
 		}
 	}
 }
